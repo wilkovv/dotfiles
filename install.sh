7db471e7d93f8f0abf2148eec8fc039da2cf1dff
Wojciech <wilkow1337@gmail.com>
2022-07-15 17:40:39 +0200

    Added automatic default wallpaper in sway

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ git clone https://github.com/romkatv/powerlevel10k $HOME/.config/zsh/powerlevel1
 git clone https://github.com/zsh-users/zsh-autosuggestions  $HOME/.config/zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting  $HOME/.config/zsh/zsh-syntax-highlighting-master
 
+sudo usermod --shell /bin/zsh wilkow
+
+cp /usr/share/background/sway/Sway_Wallpaper_Blue_1920x1080.png ~/Pictures/Wallpapers/Sway-Wallpaper.png
