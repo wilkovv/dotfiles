61c1b1cb746633a3298d02f08300958f5ec4d889
Wojciech <wilkow1337@gmail.com>
2022-07-15 16:57:36 +0200

    Fixed zsh plugins installation

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ then
 fi
 
 git clone https://github.com/romkatv/powerlevel10k $HOME/.config/zsh/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting  $HOME/.config/zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions  $HOME/.config/zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting  $HOME/.config/zsh/zsh-syntax-highlighting-master
 
