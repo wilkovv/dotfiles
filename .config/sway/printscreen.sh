13714323a9bd94b91ee73df5ed75ce7bd5650a91
Wojciech <wilkow1337@gmail.com>
2022-09-16 02:18:56 +0200

    Add selected region printscreen option

--- .config/sway/printscreen.sh
+++ .config/sway/printscreen.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 NAME=$(echo ~/Pictures/$(date +'%d_%m_%y_%H:%M:%S.png'))
-echo $NAME
-grim $NAME
-#sleep 2
+if [[ "$1" == "--select" ]]; then
+	POS=$(slurp)
+	grim -g "$POS" "$NAME"
+else
+	grim "$NAME"
+fi
 wl-copy < $NAME
